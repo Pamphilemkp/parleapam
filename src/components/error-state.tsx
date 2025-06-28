97746ea761240a608b5ea3837550207b9716df01
pamphilemkp <pamphilemkp@gmail.com>
2025-06-28 19:48:47 +0300

    fix: correct typo in text size class for error and loading states

--- src/components/error-state.tsx
+++ src/components/error-state.tsx
@@ -16,7 +16,7 @@ export const ErrorState = ({
         <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
             <AlertCircleIcon className="size-6  text-red-500" />
             <div className="flex flex-col gap-y-2 text-center">
-                <h6 className="text-llg font-medium">{title}</h6>
+                <h6 className="text-lg font-medium">{title}</h6>
                 <p className="text-sm">{description}</p>
             </div>
         </div>
--- src/components/loading-state.tsx
+++ src/components/loading-state.tsx
@@ -16,7 +16,7 @@ export const LoadingState = ({
         <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
             <Loader2Icon className="size-6 animate-spin text-primary" />
             <div className="flex flex-col gap-y-2 text-center">
-                <h6 className="text-llg font-medium">{title}</h6>
+                <h6 className="text-lg font-medium">{title}</h6>
                 <p className="text-sm">{description}</p>
             </div>
         </div>
