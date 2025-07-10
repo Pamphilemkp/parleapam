ea268d652bd91b910ae9f9edc7cb3b010de54af9
pamphilemkp <pamphilemkp@gmail.com>
2025-07-10 23:49:29 +0300

    feat: implement agent update and removal functionality, add confirmation dialog, and enhance agent form handling

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -4,3 +4,8 @@ export const AgentsInsertSchema = z.object({
     name: z.string().min(1, {message: "Name is required"}),
     instructions: z.string().min(1, {message: "Instrutions are required"}),  
 });
+
+export const AgentsUpdateSchema = AgentsInsertSchema.extend({
+    id: z.string().min(1, {message: "Id is required"}),
+});
+
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,29 +1,67 @@
 "use client";
 
 import { ErrorState } from "@/components/error-state";
+import { useState } from "react";
 import { LoadingState } from "@/components/loading-state";
 import { trpc } from "@/trpc/client";
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "../../hooks/use-confirm";
+import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
-interface props {
+interface Props {
     agentId: string;
 };
 
 
-export const AgentIdView = ({agentId}: props) => {
+export const AgentIdView = ({agentId}: Props) => {
     const { data } = trpc.agents.getOne.useQuery({ id: agentId });
+    const router = useRouter();
+    const trpcContext = trpc.useContext();
+    const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
 
+    const removeAgent = trpc.agents.remove.useMutation({
+        onSuccess: async () => {
+            await trpcContext.agents.getMany.invalidate();
+            router.push("/agents");
+        },
+        onError: (error: { message: string }) => {
+            toast.error(`Error removing agent: ${error.message}`);
+        },
+    });
+
+    const [RemoveConfirmation, confirmRemove] = useConfirm(
+        "Are you sure you want to remove this agent?",
+        `The following action will remove ${data?.meetingCount} associated meetings.`,
+    );
+
+    const handleRemoveAgent = async() => {
+        const ok = await confirmRemove();
+        if (!ok) return;
+
+        await removeAgent.mutateAsync({ id: agentId });
+    };
 
     return (
-        <div className="flex-1 px-4  py-4 md:px-8flex flex-col gap-4">
+        <>
+        <RemoveConfirmation />
+        {data && (
+          <UpdateAgentDialog
+            open={updateAgentDialogOpen}
+            onOpenChange={setUpdateAgentDialogOpen}
+            initialValues={data}
+          />
+        )}
+        <div className="flex-1 px-4  py-4 md:px-8 flex flex-col gap-4">
             <AgentIdViewHeader
                 agentId={agentId}
                 agentName={data?.name ?? ""}
-                onEdit={() => {}}
-                onRemove={() => {}}
+                onEdit={() => {setUpdateAgentDialogOpen(true)}}
+                onRemove={() => handleRemoveAgent()}
             />
             <div className="bg-white p-4 rounded-lg">
                 <div className="px-4 py-2 gap-y-5 flex flex-col col-span-5">
@@ -55,6 +93,7 @@ export const AgentIdView = ({agentId}: props) => {
                 </div>
             </div>
         </div>
+     </>
     );
 }
 
